2b27adc26d446c17333c450879e82bcf3d8e1859
Alejandro Calderon <acaldero.uc3m@gmail.com>
2016-08-07 20:11:07 +0200

    refactoring to simplify and clarify

--- wepsim_tester.js
+++ wepsim_tester.js
@@ -20,8 +20,169 @@
 
 
     //
-    //  Auxiliar UI
+    //  WepSIM checker API
     //
+
+    function load_firmware_from_files ( mfiles, tableDiv )
+    {
+	// HTML where related file information will be loaded...
+	var o = "" ;
+	for (var i=0; i<mfiles.length; i++) {
+             o = o + show_filerow(mfiles[i], i) ;
+	}
+	$(tableDiv).html(o);
+
+	// Read into the HTML generated...
+	var fileReader = new Array();
+	for (var i=0; i<mfiles.length; i++) {
+	     load_firmware_from_files_aux(mfiles, fileReader, i);
+	}
+    }
+
+    function execute_firmwares_and_asm ( checklist_text, asm_text )
+    {
+        // get the simware
+	var SIMWARE = get_simware() ;
+
+        // get the json_checklist
+	var textToConvert  = checklist_text.toLowerCase();
+        var json_checklist = read_checklist(textToConvert) ;
+
+        // loop over firmwares, execute the asm code over it
+	var i=0;
+	var ita = $("#LF"+i);
+	while (ita.length != 0)
+	{
+            // load firmware
+	    var ifirm = ita.text();
+	    var preSM = loadFirmware(ifirm);
+	    $("#BF"+i).text(JSON.stringify(preSM));
+	    if (preSM.error != null)
+	    {
+		$("#RUC"+i).text("KO");
+
+		i++;
+		ita = $("#LF"+i);
+		continue;
+	    }
+	    $("#RUC"+i).text("OK");
+            update_memories(preSM);
+
+            // load assembly
+	    var SIMWAREaddon = simlang_compile(asm_text, SIMWARE);
+	    $("#EF"+i).text(JSON.stringify(SIMWAREaddon, null, 2));
+	    $("#RE"+i).text("OK");
+	    if (SIMWAREaddon.error != null) 
+	    {
+		$("#RE"+i).text("KO");
+
+		i++;
+		ita = $("#LF"+i);
+		continue;
+	    }
+	    set_simware(SIMWAREaddon) ;
+	    update_memories(SIMWARE) ;
+
+            // execute firmware-assembly
+            init("","","","");
+	    reset() ;
+	    var reg_pc        = sim_states["REG_PC"].value ;
+	    var reg_pc_before = sim_states["REG_PC"].value - 4 ;
+	    var code_begin = parseInt(segments['.text'].begin) ;
+	    var code_end   = parseInt(segments['.text'].end) ;
+	    while ( (reg_pc != reg_pc_before) && (reg_pc < code_end) && (reg_pc >= code_begin) )
+	    {
+	       execute_microprogram() ;
+
+	       reg_pc_before = reg_pc ;
+	       reg_pc = sim_states["REG_PC"].value ;
+	    }
+
+            // compare with expected results
+            var obj_result = to_check(json_checklist) ;
+	    $("#XF"+i).text(JSON.stringify(obj_result.result, null, 2));
+	    $("#RX"+i).text(obj_result.errors);
+
+            // next firmware
+	    i++ ;
+	    ita = $("#LF"+i) ;
+	}
+    }
+
+
+    //
+    //  Auxiliar functions
+    //
+
+    function load_firmware_from_files_aux ( mfiles, fileReader, i )
+    {
+	    fileReader[i] = new FileReader();
+	    fileReader[i].onload = function (fileLoadedEvent) {
+					    var textFromFileLoaded = fileLoadedEvent.target.result;
+					    $("#LF"+i).text(textFromFileLoaded);
+					  //$("#LF"+i).css('overflow-y', 'scroll');
+					    $("#RL"+i).text('OK');
+				   };
+	    fileReader[i].readAsText(mfiles[i], "UTF-8");
+    }   
+
+    function show_filerow ( mfile, i )
+    {
+	var o = "" ;
+
+	o = o + "<tr>" +
+		"<td>" + mfile.name + "</td>" +
+		"<td>" +
+		"    <div id='LF" + i + "' style='display:none;'></div>" +
+		"    <a href='#' " + 
+		"       onclick=\"$('#IF').popup('open');" + 
+		"                 var firm=$('#LF"+i+"').text();" + 
+		"                 $('#LF').html('<pre>'+firm+'</pre>');\""+
+                "       data-position-to='windows' data-transition='none' " +
+		"       data-rel='popup'><div id='RL" + i + "'>NONE</div></a>" + 
+		"</td>" +
+		"<td>" +
+		"    <div id='BF" + i + "' style='display:none;'></div>" +
+		"    <a href='#' " + 
+		"       onclick=\"$('#IUC').popup('open');" + 
+		"                 $('#BF').html('<h1>Please &#181;check and wait...</h1>');" +
+		"                 var firm_json=$('#BF"+i+"').text();" + 
+		"                 if (firm_json == '') return;" + 
+		"                 var firm = JSON.parse(firm_json);" +
+		"                 show_firm_result('#BF', firm);\"" +
+                "       data-position-to='windows' data-transition='none' " +
+		"       data-rel='popup'><div id='RUC" + i + "'>NONE</div></a>" + 
+		"</td>" +
+		"<td>" +
+		"    <div id='EF" + i + "' style='display:none;'></div>" +
+		"    <a href='#' " + 
+		"       onclick=\"$('#IE').popup('open');" + 
+		"                 $('#EF').html('<h1>Please &#181;check and wait...</h1>');" +
+		"                 var asm_json=$('#EF"+i+"').text();" + 
+		"                 if (asm_json == '') return;" + 
+		"                 var asm = JSON.parse(asm_json);" +
+		"                 show_asm_result('#EF', asm);\"" +
+                "       data-position-to='windows' data-transition='none' " +
+		"       data-rel='popup'><div id='RE" + i + "'>NONE</div></a>" + 
+		"</td>" +
+		"<td>" +
+		"    <div id='XF" + i + "' style='display:none;'></div>" +
+		"    <a href='#' " + 
+		"       onclick=\"$('#IX').popup('open');" + 
+		"                 $('#XF').html('<h1>Please &#181;check and wait...</h1>');" +
+		"                 var chcklst_json=$('#XF"+i+"').text();" + 
+		"                 if (chcklst_json == '') return;" + 
+		"                 var chcklst = JSON.parse(chcklst_json);" +
+		"                 show_checklist_result('#XF', chcklst);\"" +
+                "       data-position-to='windows' data-transition='none' " +
+		"       data-rel='popup'><div id='RX" + i + "'>NONE</div></a>" + 
+		"</td>" +
+		"</tr>" ;
+
+        return o ;
+    }
+
+    // Auxiliar to show_filerow
 
     function show_firm_result ( jqDiv, firm )
     {
@@ -82,173 +243,42 @@
 	    $(jqDiv).html(checkreport2html(checklist)) ;
     }   
 
-    function show_filerow ( i, mfile )
-    {
-	var o = "" ;
 
-	o = o + "<tr>" +
-		"<td>" + mfile.name + "</td>" +
-		"<td>" +
-		"    <div id='LF" + i + "' style='display:none;'></div>" +
-		"    <a href='#' " + 
-		"       onclick=\"$('#IF').popup('open');" + 
-		"                 var firm=$('#LF"+i+"').text();" + 
-		"                 $('#LF').html('<pre>'+firm+'</pre>');\""+
-                "       data-position-to='windows' data-transition='none' " +
-		"       data-rel='popup'><div id='RL" + i + "'>NONE</div></a>" + 
-		"</td>" +
-		"<td>" +
-		"    <div id='BF" + i + "' style='display:none;'></div>" +
-		"    <a href='#' " + 
-		"       onclick=\"$('#IUC').popup('open');" + 
-		"                 $('#BF').html('<h1>Please &#181;check and wait...</h1>');" +
-		"                 var firm_json=$('#BF"+i+"').text();" + 
-		"                 if (firm_json == '') return;" + 
-		"                 var firm = JSON.parse(firm_json);" +
-		"                 show_firm_result('#BF', firm);\"" +
-                "       data-position-to='windows' data-transition='none' " +
-		"       data-rel='popup'><div id='RUC" + i + "'>NONE</div></a>" + 
-		"</td>" +
-		"<td>" +
-		"    <div id='EF" + i + "' style='display:none;'></div>" +
-		"    <a href='#' " + 
-		"       onclick=\"$('#IE').popup('open');" + 
-		"                 $('#EF').html('<h1>Please &#181;check and wait...</h1>');" +
-		"                 var asm_json=$('#EF"+i+"').text();" + 
-		"                 if (asm_json == '') return;" + 
-		"                 var asm = JSON.parse(asm_json);" +
-		"                 show_asm_result('#EF', asm);\"" +
-                "       data-position-to='windows' data-transition='none' " +
-		"       data-rel='popup'><div id='RE" + i + "'>NONE</div></a>" + 
-		"</td>" +
-		"<td>" +
-		"    <div id='XF" + i + "' style='display:none;'></div>" +
-		"    <a href='#' " + 
-		"       onclick=\"$('#IX').popup('open');" + 
-		"                 $('#XF').html('<h1>Please &#181;check and wait...</h1>');" +
-		"                 var chcklst_json=$('#XF"+i+"').text();" + 
-		"                 if (chcklst_json == '') return;" + 
-		"                 var chcklst = JSON.parse(chcklst_json);" +
-		"                 show_checklist_result('#XF', chcklst);\"" +
-                "       data-position-to='windows' data-transition='none' " +
-		"       data-rel='popup'><div id='RX" + i + "'>NONE</div></a>" + 
-		"</td>" +
-		"</tr>" ;
+    // TODO: get checklist ready
+    // TODO: check states, memory, etc. in order to check the expected result is accommplished
+    // TODO: "register $0 >= 100"*
+
+    function checkreport2html ( checklist )
+    {
+        var o = "" ;
+        var color = "green" ;
+
+        o += "<table border=1>" +
+             "<tr>" +
+             "<th>Element Type</th>" +
+             "<th>Element Id.</th>" +
+             "<th>Expected</th>" +
+             "<th>Obtained</th>" +
+             "</tr>" ;
+        for (var i=0; i<checklist.length; i++)
+        {
+             if (checklist[i].equals === false)
+                  color = "orange" ;
+             else color = "lightgreen" ;
+
+             o += "<tr bgcolor=" + color + ">" +
+                  "<td>" + checklist[i].elto_type + "</td>" +
+                  "<td>" + checklist[i].elto_id   + "</td>" +
+                  "<td>" + checklist[i].expected  + "</td>" +
+                  "<td>" + checklist[i].obtained  + "</td>" +
+                  "</tr>" ;
+        }
+        o += "</table>" ;
 
         return o ;
     }
 
-    function checkFirmwares ()
-    {
-	var SIMWARE = get_simware() ;
-
-	var i=0;
-	var ita = $("#LF"+i);
-	while (ita.length != 0)
-	{
-            // load firmware
-	    var ifirm = ita.text();
-	    var preSM = loadFirmware(ifirm);
-	    $("#BF"+i).text(JSON.stringify(preSM));
-	    if (preSM.error != null)
-	    {
-		$("#RUC"+i).text("KO");
-
-		i++;
-		ita = $("#LF"+i);
-		continue;
-	    }
-	    $("#RUC"+i).text("OK");
-            update_memories(preSM);
-
-            // load assembly
-	    var textToConvert = $('#asm_test').text();
-		textToConvert = textToConvert.toLowerCase();
-	    var SIMWAREaddon  = simlang_compile(textToConvert, SIMWARE);
-	    $("#EF"+i).text(JSON.stringify(SIMWAREaddon, null, 2));
-	    $("#RE"+i).text("OK");
-	    if (SIMWAREaddon.error != null) 
-	    {
-		$("#RE"+i).text("KO");
-
-		i++;
-		ita = $("#LF"+i);
-		continue;
-	    }
-	    set_simware(SIMWAREaddon) ;
-	    update_memories(SIMWARE) ;
-
-            // execute firmware-assembly
-            init("","","","");
-	    reset() ;
-	    var reg_pc        = sim_states["REG_PC"].value ;
-	    var reg_pc_before = sim_states["REG_PC"].value - 4 ;
-	    var code_begin = parseInt(segments['.text'].begin) ;
-	    var code_end   = parseInt(segments['.text'].end) ;
-	    while ( (reg_pc != reg_pc_before) && (reg_pc < code_end) && (reg_pc >= code_begin) )
-	    {
-	       execute_microprogram() ;
-
-	       reg_pc_before = reg_pc ;
-	       reg_pc = sim_states["REG_PC"].value ;
-	    }
-
-            // compare with expected results
-	    var textToConvert  = $('#checklist').text();
-		textToConvert  = textToConvert.toLowerCase();
-            var json_checklist = loadChecklist(textToConvert) ;
-            var obj_result     = to_check(json_checklist) ;
-	    $("#XF"+i).text(JSON.stringify(obj_result.result, null, 2));
-	    $("#RX"+i).text(obj_result.errors);
-
-            // next firmware
-	    i++ ;
-	    ita = $("#LF"+i) ;
-	}
-    }
-
-
-    //
-    //  Load files
-    //
-
-    function load_firmware_from_files_aux ( mfiles, fileReader, i )
-    {
-	    fileReader[i] = new FileReader();
-	    fileReader[i].onload = function (fileLoadedEvent) {
-					    var textFromFileLoaded = fileLoadedEvent.target.result;
-					    $("#LF"+i).text(textFromFileLoaded);
-					  //$("#LF"+i).css('overflow-y', 'scroll');
-					    $("#RL"+i).text('OK');
-				   };
-	    fileReader[i].readAsText(mfiles[i], "UTF-8");
-    }   
-
-    function load_firmware_from_files ( )
-    {
-	var mfiles = document.getElementById("filesToLoad").files ;
-
-	// HTML where related file information will be loaded...
-	var o = "" ;
-	for (var i=0; i<mfiles.length; i++) {
-             o = o + show_filerow(i, mfiles[i]) ;
-	}
-	$("#tbmfile").html(o);
-
-	// Read into the HTML generated...
-	var fileReader = new Array();
-	for (var i=0; i<mfiles.length; i++) {
-	     load_firmware_from_files_aux(mfiles, fileReader, i);
-	}
-    }
-
-
-    //
-    // TODO
-    //
-
-    // TODO: load checklist from text like: "register $0 >= 100"*
-    function loadChecklist ( checklist )
+    function read_checklist ( checklist )
     {
         var o = new Object() ;
         o.registers = new Object() ;
@@ -278,7 +308,6 @@
         return o ;
     }   
 
-    // TODO: check states, memory, etc. in order to check the expected result is accommplished
     function to_check ( expected_result )
     {
         var result = new Array() ;
@@ -327,36 +356,5 @@
         d.result = result ;
         d.errors = errors ;
         return d ;
-    }
-
-    // TODO: return the differences as HTML 
-    function checkreport2html ( checklist )
-    {
-        var o = "" ;
-        var color = "green" ;
-
-        o += "<table border=1>" +
-             "<tr>" +
-             "<th>Element Type</th>" +
-             "<th>Element Id.</th>" +
-             "<th>Expected</th>" +
-             "<th>Obtained</th>" +
-             "</tr>" ;
-        for (var i=0; i<checklist.length; i++)
-        {
-             if (checklist[i].equals === false)
-                  color = "orange" ;
-             else color = "lightgreen" ;
-
-             o += "<tr bgcolor=" + color + ">" +
-                  "<td>" + checklist[i].elto_type + "</td>" +
-                  "<td>" + checklist[i].elto_id   + "</td>" +
-                  "<td>" + checklist[i].expected  + "</td>" +
-                  "<td>" + checklist[i].obtained  + "</td>" +
-                  "</tr>" ;
-        }
-        o += "</table>" ;
-
-        return o ;
     }
 
