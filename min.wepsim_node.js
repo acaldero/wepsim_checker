e0ce7f202d8a13e715c04f00eed3aa015d3cc550
Alejandro Calderón <acaldero@inf.uc3m.es>
2018-01-04 18:09:23 +0100

    action added

--- min.wepsim_node.js
+++ min.wepsim_node.js
@@ -240,7 +240,7 @@ var tutorials={};tutorials.welcome={};tutorials.simpleusage={};tutorials.welcome
         var ret = wepsim_core_compile_firmware(str_firmware) ;
 	if (false == ret.ok) 
 	{
-            ret1.msg = "Firmware ERROR: " + ret.msg + ".\n" ;
+            ret1.msg = "ERROR: Firmware: " + ret.msg + ".\n" ;
             ret1.ok = false ;
 	    return ret1 ;
 	}
@@ -249,7 +249,7 @@ var tutorials={};tutorials.welcome={};tutorials.simpleusage={};tutorials.welcome
         ret = wepsim_core_compile_assembly(str_assembly) ;
 	if (false == ret.ok) 
         {
-            ret1.msg = "Assembly ERROR: " + ret.msg + ".\n" ;
+            ret1.msg = "ERROR: Assembly: " + ret.msg + ".\n" ;
             ret1.ok = false ;
 	    return ret1 ;
 	}
@@ -261,7 +261,7 @@ var tutorials={};tutorials.welcome={};tutorials.simpleusage={};tutorials.welcome
 	ret = wepsim_core_execute_asm_and_firmware(max_instructions, max_cycles) ;
 	if (true == ret.error) 
 	{
-            ret1.msg = "ERROR: cannot execute the assembly and firmware.\n" ;
+            ret1.msg = "ERROR: Execution: cannot execute the assembly and firmware.\n" ;
             ret1.ok = false ;
 	    return ret1 ;
 	}
@@ -271,7 +271,7 @@ var tutorials={};tutorials.welcome={};tutorials.simpleusage={};tutorials.welcome
 	var report1 = wepsim_core_show_checkresults(result1, "text", true) ;
 	if (result1.errors != 0) 
 	{
-            console.log("\nERROR: different results:\n" + report1 + "\n") ;
+            ret1.msg = "ERROR: Execution: different results: " + report1 + "\n" ;
             ret1.ok = false ;
 	    return ret1 ;
         }
