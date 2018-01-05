4652661455b5eb84f8cc0b738c0e77fad48ea87f
Alejandro Calderón <acaldero@inf.uc3m.es>
2018-01-05 12:12:30 +0100

    minor update

--- mk_test.sh
+++ mk_test.sh
@@ -2,14 +2,14 @@
 #set -x
 
 test_example_ok() {
-  node wepsim_node.sh check ./examples/microcode_3.txt ./examples/code_3.txt ./examples/checklist_3.txt > /tmp/k1
-  nlines=$(grep OK /tmp/k1 | wc -l)
-  assertNotSame "Failure in the correct example :-(" '1' "${nlines}"
+  node wepsim_node.sh check ./examples/microcode_3.txt ./examples/code_3.txt ./examples/checklist_3.txt > kk1
+  nlines=$(grep OK kk1 | wc -l)
+  assertNotSame "Failure in the correct example :-("   '1' "${nlines}"
 }
 
 test_example_ko() {
-  node wepsim_node.sh check ./examples/microcode_3.txt ./examples/code_3.txt ./examples/checklist_2.txt > /tmp/k1
-  nlines=$(grep ERROR /tmp/k1 | wc -l)
+  node wepsim_node.sh check ./examples/microcode_3.txt ./examples/code_3.txt ./examples/checklist_2.txt > kk1
+  nlines=$(grep ERROR kk1 | wc -l)
   assertNotSame "Failure in the incorrect example :-(" '1' "${nlines}"
 }
 
