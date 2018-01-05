f3f0b21a18fe48ae524b0d80dd9c5bc2a89a789e
Alejandro Calderón <acaldero@inf.uc3m.es>
2018-01-05 12:15:38 +0100

    minor update

--- mk_test.sh
+++ mk_test.sh
@@ -3,14 +3,14 @@
 
 test_example_ok() {
   node wepsim_node.sh check ./examples/microcode_3.txt ./examples/code_3.txt ./examples/checklist_3.txt > kk1
-  nlines=$(grep OK kk1 | wc -l)
-  assertNotSame "Failure in the correct example :-("   '1' "${nlines}"
+  nlines=$(grep ERROR kk1 | wc -l)
+  assertNotSame "In the correct example :-("   '0' "${nlines}"
 }
 
 test_example_ko() {
   node wepsim_node.sh check ./examples/microcode_3.txt ./examples/code_3.txt ./examples/checklist_2.txt > kk1
-  nlines=$(grep ERROR kk1 | wc -l)
-  assertNotSame "Failure in the incorrect example :-(" '1' "${nlines}"
+  nlines=$(grep OK kk1 | wc -l)
+  assertNotSame "In the incorrect example :-(" '0' "${nlines}"
 }
 
 . shunit2-2.0.3/src/shell/shunit2
